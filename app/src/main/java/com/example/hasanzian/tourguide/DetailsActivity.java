339faab0dc8ef0b56f21f690715020b9415d5c6b
hasanmohdkhan <hasanzian@gmail.com>
2018-07-26 23:32:17 +0530

    added animation,detailsActivity

--- app/src/main/java/com/example/hasanzian/tourguide/DetailsActivity.java
+++ app/src/main/java/com/example/hasanzian/tourguide/DetailsActivity.java
@@ -1,13 +1,46 @@
 package com.example.hasanzian.tourguide;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 public class DetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
+        setContentView(R.layout.details_more_details);
+        ImageView imageView = findViewById(R.id.backgroundImage);
+        TextView placeName = findViewById(R.id.title);
+        TextView info = findViewById(R.id.description);
+        YoYo.with(Techniques.SlideInLeft)
+                .duration(1700).playOn(findViewById(R.id.title));
+
+        YoYo.with(Techniques.SlideInUp)
+                .duration(1700).playOn(findViewById(R.id.description));
+
+        Bundle bundle = getIntent().getExtras();
+        String titleFromList = bundle.getString("T");
+        String infoFromList = bundle.getString("I");
+        int idFromList = bundle.getInt("ID");
+
+        placeName.setText(titleFromList);
+        info.setText(infoFromList);
+        imageView.setImageResource(idFromList);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/hasanzian/tourguide/fragments/SpotsFragment.java
+++ app/src/main/java/com/example/hasanzian/tourguide/fragments/SpotsFragment.java
@@ -1,14 +1,18 @@
 package com.example.hasanzian.tourguide.fragments;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.hasanzian.tourguide.Adapters.TourAdaptor;
+import com.example.hasanzian.tourguide.DetailsActivity;
 import com.example.hasanzian.tourguide.R;
 import com.example.hasanzian.tourguide.datamModel.Model;
 
@@ -26,7 +30,7 @@ public class SpotsFragment extends Fragment{
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_spot, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_spot, container, false);
 
         ListView listView = rootView.findViewById(R.id.list);
         List<Model> list = new ArrayList<>();
@@ -35,9 +39,39 @@ public class SpotsFragment extends Fragment{
         list.add((new Model(getString(R.string.khajuraho_str),getString(R.string.khajuraho_info),R.drawable.khujaro)));
         list.add((new Model(getString(R.string.chitrakoot_str),getString(R.string.chittrakoot_str),R.drawable.chiratkoot)));
 
-        TourAdaptor adaptor = new TourAdaptor(getActivity(),list);
+        final TourAdaptor adaptor = new TourAdaptor(getActivity(), list);
         listView.setAdapter(adaptor);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent detailIntent = new Intent(getContext(), DetailsActivity.class);
+
+//                Pair<View, String>[] pairs = new Pair[1];
+//
+//                pairs[0] = new Pair<View, String>(rootView.findViewById(R.id.image), getString(R.string.trans_name));
+//
+//                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(getActivity(), pairs);
+//                startActivity(detailIntent, options.toBundle());
+
+                Model current = adaptor.getItem(position);
+                String title = current.getPlaceName();
+                String infoPlace = current.getPlaceInfo();
+                int imageId = current.getImageResource();
+
+                detailIntent.putExtra("T", title);
+                detailIntent.putExtra("I", infoPlace);
+                detailIntent.putExtra("ID", imageId);
+
+                View sharedView = rootView.findViewById(R.id.item);
+                String transitionName = getString(R.string.trans_name);
+
+                ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(getActivity(), sharedView, transitionName);
+                startActivity(detailIntent, transitionActivityOptions.toBundle());
+
+
+            }
+        });
 
         return rootView;
     }
